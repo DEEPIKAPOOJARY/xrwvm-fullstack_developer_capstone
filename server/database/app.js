bc39624a9b4c96b44153a61074e95f0205c74d49
DEEPIKAPOOJARY <deepikapoojary2003@gmail.com>
2025-11-05 05:20:18 -0500

    Added fetchDealers, fetchDealers by state, and fetchDealer by ID endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,44 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealers = await Dealerships.find();
+        res.json(dealers);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const stateName = req.params.state;
+        const dealers = await Dealerships.find({ state: stateName });
+    
+        if (dealers.length === 0) {
+          return res.status(404).json({ message: `No dealerships found in ${stateName}` });
+        }
+    
+        res.json(dealers);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships by state' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const dealerId = parseInt(req.params.id);
+        const dealer = await Dealerships.findOne({ id: dealerId });
+    
+        if (!dealer) {
+          return res.status(404).json({ message: `Dealer with ID ${dealerId} not found` });
+        }
+    
+        res.json(dealer);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealership by ID' });
+      }
 });
 
 //Express route to insert review
